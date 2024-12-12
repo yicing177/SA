9b1401102d97fda8f11c9dda576dd5d75d7be05c
erinlii <erinkate1218@gmail.com>
2024-12-12 10:18:05 +0800

    classes-v2

--- SA/src/main/java/com/example/demo/Order.java
+++ SA/src/main/java/com/example/demo/Order.java
@@ -31,7 +31,7 @@ public class Order {
         return orderContent;
     }
 
-    public void setOrderContent(String ordercCntent) {
+    public void setOrderContent(String ordercContent) {
         this.orderContent = orderContent;
     }
 
